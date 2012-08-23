66af7898e03d609bf23c9edb2f67d06741736a1a
Adrien Di Pasquale <adrien.dipasquale@gmail.com>
2012-08-23 11:27:54 +0200

    functional version of friendsrequest

--- joshfire.connect.friendsrequest.js
+++ joshfire.connect.friendsrequest.js
@@ -2,8 +2,13 @@ define([], function () {
   return function (config) {
     return {
       startActivity: function (intent, successCallback, failureCallback) {
+        var fbTo = "";
+        if(intent.data.facebookIds && intent.data.facebookIds.length > 0) {
+          fbTo = intent.data.facebookIds.join(",");
+        }
         FB.ui({method: 'apprequests',
-          message: 'Viens jouer au bingo !'
+          message: intent.data.message,
+          to: fbTo
         }, function(){});
         return successCallback(intent.data);
       }
