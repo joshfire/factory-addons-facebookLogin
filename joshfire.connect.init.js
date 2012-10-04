4b07c0b13b763f6adc6d9abcdc30c3e2553130a8
Nicolas Lesconnec <nicolas.lesconnec.pro@gmail.com>
2012-10-04 09:08:37 +0200

    Handle localisation of facebook connect sdk Will only use en_US if no intent.data.locale is given by the app Caveat : We could improve that by checking the intent.data.locale validity ... but it would imply to ensure being synchro with facebook supported locales https://www.facebook.com/translations/FacebookLocales.xml

--- joshfire.connect.init.js
+++ joshfire.connect.init.js
@@ -94,7 +94,7 @@ define([], function () {
              var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
              if (d.getElementById(id)) {return;}
              js = d.createElement('script'); js.id = id; js.async = true;
-             js.src = "//connect.facebook.net/en_US/all.js";
+             js.src = "//connect.facebook.net/"+(intent.data.locale || 'en_US')+"/all.js";
              ref.parentNode.insertBefore(js, ref);
            }(document));
 
